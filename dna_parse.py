8e0e4fbed07967b8d3776567bc34f217fb2e9af9
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-06 13:43:25 +0000

    finished DnaParse class

--- dna_parse.py
+++ dna_parse.py
@@ -1,6 +1,6 @@
 class DnaParse:
-    def __init__(self, max, dna_sequence):
-        self.max = max
+    def __init__(self, max_length, dna_sequence):
+        self.max_length = max_length
         self.dna_sequence = dna_sequence
         self.a_num = 0
         self.c_num = 0
@@ -8,20 +8,36 @@ class DnaParse:
         self.t_num = 0
 
     def length_check(self):
-        if len(self.dna_sequence) > self.max:
-            return f"This sequence is too long to parse. Please provide a sequence shorter than {self.max} " \
+        if len(self.dna_sequence) > self.max_length:
+            return f"This sequence is too long to parse. Please provide a sequence shorter than {self.max_length} " \
                    f"characters long."
         else:
             return "We can parse this sequence!"
 
     def loop_through(self):
-        for _ in self.dna_sequence:
-            if _.upper() == "A":
-                self.a_num += 1
-            elif _.upper() == "C":
-                self.c_num += 1
-            elif _.upper() == "G":
-                self.g_num += 1
-            elif _.upper() == "T":
-                self.t_num += 1
+        if self.length_check() == "We can parse this sequence!":
+            for _ in self.dna_sequence:
+                if _.upper() == "A":
+                    self.a_num += 1
+                elif _.upper() == "C":
+                    self.c_num += 1
+                elif _.upper() == "G":
+                    self.g_num += 1
+                elif _.upper() == "T":
+                    self.t_num += 1
+            print(self.display_output())
+        else:
+            print(self.length_check())
+
+
+    def display_output(self):
+        return f"Here is a report of how often each letter appears in the provided string:\n" \
+               f"A: {self.a_num}, " \
+               f"C: {self.c_num}, " \
+               f"G: {self.g_num}, " \
+               f"T: {self.t_num}"
+
+
+dna_test = DnaParse(1000, "ACTTTGCGCCGAGACAGATATATACACAGAAAGAACACATATAAATTATTCGGCGCTCTACTAGCGGCTA")
+dna_test.loop_through()
 
